abba4e37048eb0e6271102349639ea73a81526f6
gareth.cross <gareth.cross@flyzipline.com>
2022-06-28 21:50:18 -0700

    jinja_env supports specifying external search paths

--- symforce/codegen/template_util.py
+++ symforce/codegen/template_util.py
@@ -138,11 +138,13 @@ def add_preamble(source: str, name: Path, comment_prefix: str) -> str:
 
 
 @functools.lru_cache
-def jinja_env(template_dir: T.Openable) -> RelEnvironment:
+def jinja_env(template_dir: T.Openable, search_paths: T.Iterable[T.Openable] = ()) -> RelEnvironment:
     """
     Helper function to cache the Jinja environment, which enables caching of loaded templates
     """
-    loader = jinja2.FileSystemLoader(os.fspath(template_dir))
+    all_search_paths = [os.fspath(template_dir)]
+    all_search_paths.extend(search_paths)
+    loader = jinja2.FileSystemLoader(searchpath=all_search_paths)
     env = RelEnvironment(
         loader=loader,
         trim_blocks=True,
@@ -160,9 +162,10 @@ def render_template(
     template_dir: T.Openable,
     output_path: T.Optional[T.Openable] = None,
     autoformat: bool = True,
+    search_paths: T.Iterable[T.Openable] = ()
 ) -> str:
     """
-    Boiler plate to render template. Returns the rendered string and optionally writes to file.
+    Boilerplate to render template. Returns the rendered string and optionally writes to file.
 
     Args:
         template_path: file path of the template to render
@@ -170,6 +173,7 @@ def render_template(
         template_dir: Base directory where templates are found
         output_path: If provided, writes to file
         autoformat: Run a code formatter on the generated code
+        search_paths: Additional directories jinja should search when resolving imports.
     """
     if not isinstance(template_path, Path):
         template_path = Path(template_path)
@@ -183,7 +187,7 @@ def render_template(
 
     filetype = FileType.from_template_path(Path(template_path))
 
-    template = jinja_env(template_dir).get_template(os.fspath(template_path))
+    template = jinja_env(template_dir, search_paths=search_paths).get_template(os.fspath(template_name))
     rendered_str = add_preamble(
         str(template.render(**data)),
         template_path,
@@ -249,16 +253,15 @@ class TemplateList:
             )
         )
 
-    def render(self, autoformat: bool = True) -> T.List[str]:
+    def render(self, autoformat: bool = True, search_paths: T.Iterable[T.Openable] = ()) -> T.List[str]:
         rendered_templates = []
         for entry in self.items:
-            rendered_templates.append(
-                render_template(
-                    template_path=entry.template_path,
-                    output_path=entry.output_path,
-                    data=entry.data,
-                    template_dir=entry.template_dir,
-                    autoformat=autoformat,
-                )
-            )
+            rendered_templates.append(render_template(
+                template_path=entry.template_path,
+                output_path=entry.output_path,
+                data=entry.data,
+                template_dir=self.template_dir,
+                autoformat=autoformat,
+                search_paths=search_paths,
+            ))
         return rendered_templates
